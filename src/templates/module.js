de410a64beaaf2df941415a4e13b602ed706ba32
aguzmanSB <aguzman@silverbackstrategies.com>
2020-05-12 08:55:46 -0400

    added acf custom field

--- src/templates/module.js
+++ src/templates/module.js
@@ -9,6 +9,12 @@ const FeaturedImage = styled.img`
 export default ({pageContext}) => (
   <Layout>
     <h1>{pageContext.title}</h1>
+    <strong>
+    Website URL:
+    </strong>
+    <a href={pageContext.acf.module_url} target="_blank">
+    {pageContext.acf.module_url}
+    </a>
     <FeaturedImage src={pageContext.featured_media.source_url}/>
     <div dangerouslySetInnerHTML={{__html: pageContext.content}}/>
   </Layout>
